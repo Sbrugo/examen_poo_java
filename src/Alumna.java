fcd3b1c79ea0f809e3d6541503ac7d0cb9c9679c
Sofi Brugo <brugosofi@gmail.com>
2023-08-14 22:41:48 -0300

    first commit

---
+++ src/Alumna.java
@@ -0,0 +1,10 @@
+public class Alumna {
+    public Alumna() {
+    }
+    private String ganarPrivado(){
+        return "Ganar";
+    }
+    public String ganar(){
+        return "Ganarle a todos";
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Alumna alumna = new Alumna();
+        alumna.ganar();
+        extendsAlumna alumna2 = new extendsAlumna();
+        alumna2.
+    }
+}
